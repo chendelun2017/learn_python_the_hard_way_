df1a0e0be458c85a8269ba953ce98505c576869e
[985138146@qq.com] <[985138146@qq.com]>
2018-09-28 20:46:05 +0800

    write in 2018/9/28, today is a funny question game, everyone could download it and play for fun

--- ex33.py
+++ ex33.py
@@ -1,14 +1,50 @@
+# i = 0
+# numbers = []
+
+# while i < 6:
+#     print("At the top i is %d" % i)
+#     numbers.append(i)
+#
+#     i = i + 1
+#     print("Numbers now:", numbers)
+#     print("At the bottom i is %d" % i)
+#
+# print("The numbers: ")
+# for num in numbers:
+#     print(num)
+#
+
+# def make_list(ran, step):
+#     i = 0
+#     numbers = []
+#     ran1 = range(0, ran)
+#     for i in ran1:
+#         print("At the top i is %d" % i)
+#         numbers.append(i)
+#
+#         print("Numbers now:", numbers)
+#         print("At the bottom i is %d" % i)
+#     return numbers
+#
+# numbers = make_list(6, 1)
+# print("The numbers:")
+#
+# for num in numbers:
+#     print(num)
+
+
 i = 0
 numbers = []
 
-while i < 6:
+for i in range(0,6):
     print("At the top i is %d" % i)
     numbers.append(i)
 
     i = i + 1
-    print("Numbers now:", numbers)
+    print("Numbers now: ", numbers)
     print("At the bottom i is %d" % i)
 
 print("The numbers: ")
+
 for num in numbers:
     print(num)
